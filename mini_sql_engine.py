13691ddfea0f9bbd7cea6269e5df02966a91e85e
Sai Sukrutha <14861646+Sukku1@users.noreply.github.com>
2019-02-01 11:40:47 +0530

    where condition and join of 2 tables

--- mini_sql_engine.py
+++ mini_sql_engine.py
@@ -54,14 +54,13 @@ def parse_query(query):
     identifier_list = []
     for ele in all_list:
         identifier_list.append(str(ele))
-    # print(identifier_list)
+    #print(identifier_list)
     get_data(identifier_list)
 
 
 def get_data(iden_list):
-    if(len(iden_list) >= 5):
+    if(len(iden_list) >= 4):
         #Min query-"select * from table1 ;"
-        ##TODO
 
         #Reading table name ( tablename after from )
         from_index=2
@@ -86,24 +85,87 @@ def get_data(iden_list):
         attributes=iden_list[1].split(',')
         #print(attributes)
 
-        ###TODO: from table1,table2 - done as just printing but actually join
         ##where clause
+        conds=[]
         if("where" in iden_list[-1] or "WHERE" in iden_list[-1]):
             conds,and_flag,or_flag=process_where(iden_list[-1])
 
+        #cond=['col=value','col<value','table1.col=table2.col,...]
+        #conditions=[[0/1,col_no,op,value/col_no],[0/1,col_no,op,value/col_no],...]
+        #0-col=val like [0,col_no,op,value], 1-table.col=table.col  like [1,col_no,op,col_no]
+        conditions=[]
+        op=""
+        for condition in conds:
+            condition=condition.strip(';')
+            # print(condition)
+            if( "<" in condition):
+                op="<"
+            elif( ">" in condition):
+                op=">"
+            elif( "<=" in condition):
+                op="<="
+            elif( ">=" in condition):
+                op=">="
+            elif( "=" in condition):
+                op="=="
 
+            if(op == "=="):
+                col_name,part_2=condition.split('=')
+            elif(op):
+                col_name,part_2=condition.split(op)
+            else:
+                error_msg("Error:Invalid Syntax")
+
+            col_no=get_col_no(tables,col_name)
+
+            temp=[]
+            if(part_2.isdigit()):
+                value=part_2
+                temp.append(0)
+                temp.append(col_no)
+                temp.append(op)
+                temp.append(value)
+                conditions.append(temp)
+            else:
+                col_no2=get_col_no(tables,part_2)
+                temp.append(1)
+                temp.append(col_no)
+                temp.append(op)
+                temp.append(col_no2)
+                conditions.append(temp)
+        #print(conditions)
+
+
+        #If select '*'
         if(attributes[0] == '*'):
             if(len(tables) == 1):
-                for col in metadata[tables[i]]:
-                    print(col+'\t',end='')
-                print('\n')
-
-                file_name=FILES_PATH+tables[i].strip()+".csv"
+                table=tables[0]
+                for col in metadata[table]:
+                    sys.stdout.write(col+'\t')
+                sys.stdout.write('\n')
+                file_name=FILES_PATH+table.strip()+".csv"
                 file_data=read_csv_file(file_name)
-                for row in file_data:
-                    for i in row:
-                        print(i+'\t',end='')
-                    print('\n')
+                if(len(conds)== 0):
+                    #No where condition
+                    for row in file_data:
+                        for i in row:
+                            sys.stdout.write(i+'\t')
+                        sys.stdout.write('\n')
+                else:
+                    #where condition
+                    for row in file_data:
+                        res,flag=check_cond(conditions,and_flag,or_flag,row)
+                        if(res):
+                            if(flag == 0):
+                                for i in row:
+                                    sys.stdout.write(i+'\t')
+                                sys.stdout.write('\n')
+                            else:
+                                for i in row:
+                                    if( i != col_no2 ):
+                                        sys.stdout.write(i+'\t')
+                                sys.stdout.write('\n')
+
 
             else:
                 ##Join
@@ -124,21 +186,49 @@ def get_data(iden_list):
                     col_name="table2"+"."+col
                     column_names.append(col_name)
 
-                for col in column_names:
-                    print(col+'  ',end='')
-                print('\n')
+                ######
+                # for col in column_names:
+                #     sys.stdout.write(col+'  ')
+                # sys.stdout.write('\n')
                 
                 file_data=[]
                 for row1 in file_data1:
                     for row2 in file_data2:
                         file_data.append(row1+row2)
                         
-                for row in file_data:
-                    for i in row:
-                        print(i+'\t',end='')
-                    print('\n')
-
-
+                if(len(conds)== 0):
+                    #No where condition
+                    for row in file_data:
+                        for i in row:
+                            sys.stdout.write(i+'\t')
+                        sys.stdout.write('\n')
+                else:
+                    #where condition
+                    count = 0
+                    for row in file_data:
+                        res,flag=check_cond(conditions,and_flag,or_flag,row)
+                        if(res):
+                            if(flag == 0):
+                                if(count == 0):
+                                    for col in column_names:
+                                        sys.stdout.write(col+'  ')
+                                    sys.stdout.write('\n')
+                                    count=1
+                                for i in row:
+                                    sys.stdout.write(i+'\t')
+                                sys.stdout.write('\n')
+                            else:
+                                if(count == 0):
+                                    for col in range(0,len(column_names)):
+                                        if( col != col_no2):
+                                            sys.stdout.write(column_names[col]+'  ')
+                                    sys.stdout.write('\n')
+                                    count=1
+                                    
+                                for i in range(0,len(row)):
+                                    if( i != col_no2 ):
+                                        sys.stdout.write(row[i]+'\t')
+                                sys.stdout.write('\n')
                     
         ##Aggregate Functions- max,min,sum,avg
         elif(("max" in attributes[0]) or ("min" in attributes[0]) or ("avg" in attributes[0]) or ("sum" in attributes[0])):
@@ -162,7 +252,6 @@ def get_data(iden_list):
                 col_no=metadata[table].index(col)
 
             if("max" in attributes[0]):
-                # print("max(",table,".",col,")")
                 print("max(%s.%s)" %(table,col))
                 file_name=FILES_PATH+table.strip()+".csv"
                 file_data=read_csv_file(file_name)
@@ -215,10 +304,10 @@ def get_data(iden_list):
                 table = tables[0].strip()
 
             attrs=iden_list[2].split(',')
-            print(attrs)
+            # print(attrs)
             column_nos = []
             for i in attrs:
-                print("distinct(%s)\t"%i,end='')
+                print("distinct(%s)\t"%i)
                 if i in metadata[table]:
                     column_nos.append(metadata[table].index(i))
                 else:
@@ -237,27 +326,45 @@ def get_data(iden_list):
             # print(distinct_values)
             for row in distinct_values:
                 for i in list(row):
-                    print(i+'\t\t',end='')
-                print('\n')
+                    sys.stdout.write(i+'\t\t')
+                sys.stdout.write('\n')
 
+        #Normal query- select A,B ...
         else:
             if (len(tables)==1):
                 table = tables[0]
-                column_nos = []
+                col_nos = []
                 for i in attributes:
-                    print(i+'\t',end='')
+                    sys.stdout.write(i+'\t')
                     if i in metadata[table]:
-                        column_nos.append(metadata[table].index(i))
+                        col_nos.append(metadata[table].index(i))
                     else:
                         msg="Error: attribute doesn't exist"
                         error_msg(msg)
-                print('\n')
+                sys.stdout.write('\n')
                 file_name=FILES_PATH+table.strip()+".csv"
                 file_data=read_csv_file(file_name)
-                for row in file_data:
-                    for i in column_nos:
-                        print(row[i]+'\t',end='')
-                    print('\n')
+                if(len(conds)== 0):
+                    #No where condition
+                    for row in file_data:
+                        for i in col_nos:
+                            sys.stdout.write(row[i]+'\t')
+                        sys.stdout.write('\n')
+                else:
+                    #where condition
+                    for row in file_data:
+                        res,flag=check_cond(conditions,and_flag,or_flag,row)
+                        if(res):
+                            if(flag == 0):
+                                for i in col_nos:
+                                    sys.stdout.write(row[i]+'\t')
+                                sys.stdout.write('\n')
+                            else:
+                                for i in col_nos:
+                                    if( i != col_no2 ):
+                                        sys.stdout.write(row[i]+'\t')
+                                sys.stdout.write('\n')
+
             else:
                 ##Join
                 if(len(tables) > 2):
@@ -282,6 +389,7 @@ def get_data(iden_list):
 
                 #Which cols to print
                 col_nos=[]
+                table=None
                 for a_i in range(0,len(attributes)):
                     found=0
                     if ( "." in attributes[a_i]):
@@ -297,33 +405,111 @@ def get_data(iden_list):
                                     msg=("Error:Ambiguous attribute %s"%attributes[a_i])
                                     error_msg(msg)
                                 found=1
+                    if(table == None):
+                        error_msg("Error:Invalid Syntax")
+
                     if(table == table1):
                         col_nos.append(col_no)
                     elif(table == table2):
                         col_nos.append(len_table1+col_no)
 
                 for col in col_nos:
-                    print(column_names[col]+'  ',end='')
-                print('\n')
+                    sys.stdout.write(column_names[col]+'  ')
+                sys.stdout.write('\n')
                 
                 file_data=[]
                 for row1 in file_data1:
                     for row2 in file_data2:
                         file_data.append(row1+row2)
                         
-                for row in file_data:
-                    for i in col_nos:
-                        print(row[i]+'\t',end='')
-                    print('\n')
+                if(len(conds)== 0):
+                    #No where condition
+                    for row in file_data:
+                        for i in col_nos:
+                            sys.stdout.write(row[i]+'\t')
+                        sys.stdout.write('\n')
+                else:
+                    #where condition
+                    for row in file_data:
+                        res,flag=check_cond(conditions,and_flag,or_flag,row)
+                        if(res):
+                            if(flag == 0):
+                                for i in col_nos:
+                                    sys.stdout.write(row[i]+'\t')
+                                sys.stdout.write('\n')
+                            else:
+                                for i in col_nos:
+                                    if( i != col_no2):
+                                        sys.stdout.write(row[i]+'\t')
+                                sys.stdout.write('\n')
                 
     else:
-        msg="Invalid Syntax"
+        msg="Error:Invalid Syntax(length)"
         error_msg(msg)
 
-def process_where(where_cond):
 
+def get_col_no(tables,col_name):
+    #col_no of given attribute in condition
+    col_no=0
+    found=0
+    if ( "." in col_name):
+        table,col=col_name.split('.')
+        col_no=metadata[table].index(col)
+    else:
+        for i in range(0,len(tables)):
+            if(col_name in metadata[tables[i]]):
+                table=tables[i]
+                col=col_name
+                col_no=metadata[table].index(col)
+                if(found == 1):
+                    msg=("Error:Ambiguous attribute %s"%col_name)
+                    error_msg(msg)
+                found=1
+    #If join (col_no changes)- only 2 tables
+    if(len(tables)>1):
+        table1=tables[0]
+        table2=tables[1]
+        len_table1=len(metadata[table1])
+        if(table == table2):
+            col_no=len_table1+col_no
+    return col_no
+
+
+
+def check_cond(conditions,and_flag,or_flag,row):
+    res=True
+    ans=[]
+    flag=None  #if join cond- to not print repeated columns
+    for condition in conditions:
+        if(condition[0] == 0):
+            col_no=condition[1]
+            op=condition[2]
+            value=condition[3]
+            ans.append(eval(row[col_no]+op+value))
+            if( not flag ):
+                flag=0
+        if(condition[0] == 1):
+            col_no=condition[1]
+            op=condition[2]
+            col_no2=condition[3]
+            ans.append(eval(row[col_no]+op+row[col_no2]))
+            flag=1
+    if(and_flag):
+        res=ans[0]
+        for a in range(1,len(ans)):
+            res= res and ans[a]
+    elif(or_flag):
+        res=ans[0]
+        for a in range(1,len(ans)):
+            res= res or ans[a]
+    else:
+        res=ans[0]
+    # print(res)
+    return res,flag
+        
+
+def process_where(where_cond):
     cond_list=where_cond.split(" ")
-    print(cond_list)
     #Maximum one operator (2 conditions)
     conds=[]
     and_flag=0
@@ -345,14 +531,9 @@ def process_where(where_cond):
     if( and_flag and or_flag):
         msg="Error:one and/or operator is allowed"
         error_msg(msg)
-
-    print(conds)
+    # print(conds)
     return conds,and_flag,or_flag
 
-
-    #conditions
-
-    #TODO:joins
 
 
 def read_csv_file(file_path):
